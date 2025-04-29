fb9da6e6a5bb9cfbdba4d536790cd4936148a368
ntgengyf <53422151+ntgengyf@users.noreply.github.com>
2025-04-29 10:56:10 +0800

    Update superspeed.sh

--- superspeed.sh
+++ superspeed.sh
@@ -74,7 +74,8 @@ checkwget() {
 checkspeedtest() {
 	if  [ ! -e './speedtest-cli/speedtest' ]; then
 		echo "正在安装 Speedtest-cli"
-		wget --no-check-certificate -qO speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-$(uname -m)-linux.tgz > /dev/null 2>&1
+		#wget --no-check-certificate -qO speedtest.tgz https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-$(uname -m).tgz > /dev/null 2>&1
+		wget --no-check-certificate -qO speedtest.tgz https://filedown.me/Linux/Tool/speedtest_cli/ookla-speedtest-1.0.0-$(uname -m)-linux.tgz > /dev/null 2>&1
 	fi
 	mkdir -p speedtest-cli && tar zxvf speedtest.tgz -C ./speedtest-cli/ > /dev/null 2>&1 && chmod a+rx ./speedtest-cli/speedtest
 }
@@ -109,8 +110,9 @@ speed_test(){
 preinfo() {
 	echo "———————————————————SuperSpeed 全面测速版——————————————————"
 	echo "       bash <(curl -Lso- https://git.io/superspeed)"
-	echo "       全部节点列表:  https://git.io/superspeedList"
-	echo "       节点更新: 2020/04/09  | 脚本更新: 2020/04/09"
+	echo "       节点列表:  https://www.speedtest.net/speedtest-servers-static.php"
+	#https://github.com/spiritLHLS/speedtest.net-CN-ID/tree/main
+	echo "       节点更新: 2025/04/29  | 脚本更新: 2020/04/09"
 	echo "——————————————————————————————————————————————————————————"
 }
 
@@ -136,6 +138,10 @@ runtest() {
 		start=$(date +%s) 
 
 		 speed_test '3633' '上海' '电信'
+		 speed_test '36663' '江苏镇江５Ｇ' '电信'
+		 speed_test '5396' '江苏苏州５Ｇ' '电信'
+		 speed_test '59387' '浙江５Ｇ' '电信'
+		 speed_test '59386' '浙江５Ｇ' '电信'
 		 speed_test '24012' '内蒙古呼和浩特' '电信'
 		 speed_test '27377' '北京５Ｇ' '电信'
 		 speed_test '29026' '四川成都' '电信'
@@ -145,7 +151,6 @@ runtest() {
 		# speed_test '27810' '广西南宁' '电信'
 		 speed_test '27575' '新疆乌鲁木齐' '电信'
 		# speed_test '26352' '江苏南京５Ｇ' '电信'
-		 speed_test '5396' '江苏苏州５Ｇ' '电信'
 		# speed_test '5317' '江苏连云港５Ｇ' '电信'
 		# speed_test '7509' '浙江杭州' '电信'
 		 speed_test '23844' '湖北武汉' '电信'
@@ -154,14 +159,14 @@ runtest() {
 		 speed_test '3973' '甘肃兰州' '电信'
 		# speed_test '19076' '重庆' '电信'
 		#***
-		# speed_test '21005' '上海' '联通'
 		 speed_test '24447' '上海５Ｇ' '联通'
+		 speed_test '43752' '北京５Ｇ' '联通'
+		# speed_test '21005' '上海' '联通'
 		# speed_test '5103' '云南昆明' '联通'
 		 speed_test '5145' '北京' '联通'
 		# speed_test '5505' '北京' '联通'
 		# speed_test '9484' '吉林长春' '联通'
 		 speed_test '2461' '四川成都' '联通'
-		 speed_test '27154' '天津５Ｇ' '联通'
 		# speed_test '5509' '宁夏银川' '联通'
 		# speed_test '5724' '安徽合肥' '联通'
 		# speed_test '5039' '山东济南' '联通'
@@ -178,54 +183,14 @@ runtest() {
 		# speed_test '31985' '重庆' '联通'
 		 speed_test '4863' '陕西西安' '联通'
 		#***
-		# speed_test '30154' '上海' '移动'
-		# speed_test '25637' '上海５Ｇ' '移动'
-		# speed_test '26728' '云南昆明' '移动'
-		# speed_test '27019' '内蒙古呼和浩特' '移动'
-		 speed_test '30232' '内蒙呼和浩特５Ｇ' '移动'
-		# speed_test '30293' '内蒙古通辽５Ｇ' '移动'
-		 speed_test '25858' '北京' '移动'
-		 speed_test '16375' '吉林长春' '移动'
-		# speed_test '24337' '四川成都' '移动'
-		 speed_test '17184' '天津５Ｇ' '移动'
-		# speed_test '26940' '宁夏银川' '移动'
-		# speed_test '31815' '宁夏银川' '移动'
-		# speed_test '26404' '安徽合肥５Ｇ' '移动'
-		 speed_test '27151' '山东临沂５Ｇ' '移动'
-		# speed_test '25881' '山东济南５Ｇ' '移动'
-		# speed_test '27100' '山东青岛５Ｇ' '移动'
-		# speed_test '26501' '山西太原５Ｇ' '移动'
-		 speed_test '31520' '广东中山' '移动'
-		# speed_test '6611' '广东广州' '移动'
-		# speed_test '4515' '广东深圳' '移动'
-		# speed_test '15863' '广西南宁' '移动'
-		# speed_test '16858' '新疆乌鲁木齐' '移动'
-		 speed_test '26938' '新疆乌鲁木齐５Ｇ' '移动'
-		# speed_test '17227' '新疆和田' '移动'
-		# speed_test '17245' '新疆喀什' '移动'
-		# speed_test '17222' '新疆阿勒泰' '移动'
-		# speed_test '27249' '江苏南京５Ｇ' '移动'
-		# speed_test '21845' '江苏常州５Ｇ' '移动'
-		# speed_test '26850' '江苏无锡５Ｇ' '移动'
-		# speed_test '17320' '江苏镇江５Ｇ' '移动'
-		 speed_test '25883' '江西南昌５Ｇ' '移动'
-		# speed_test '17223' '河北石家庄' '移动'
-		# speed_test '26331' '河南郑州５Ｇ' '移动'
-		# speed_test '6715' '浙江宁波５Ｇ' '移动'
-		# speed_test '4647' '浙江杭州' '移动'
-		# speed_test '16503' '海南海口' '移动'
-		# speed_test '28491' '湖南长沙５Ｇ' '移动'
-		# speed_test '16145' '甘肃兰州' '移动'
-		 speed_test '16171' '福建福州' '移动'
-		# speed_test '18444' '西藏拉萨' '移动'
-		 speed_test '16398' '贵州贵阳' '移动'
-		 speed_test '25728' '辽宁大连' '移动'
-		# speed_test '16167' '辽宁沈阳' '移动'
-		# speed_test '17584' '重庆' '移动'
-		# speed_test '26380' '陕西西安' '移动'
-		# speed_test '29105' '陕西西安５Ｇ' '移动'
-		# speed_test '29083' '青海西宁５Ｇ' '移动'
-		# speed_test '26656' '黑龙江哈尔滨' '移动'
+		 speed_test '4575' '成都' '移动'
+		 speed_test '16204' '江苏苏州' '移动'
+		 speed_test '54312' '杭州 5G' '移动'
+		 speed_test '60584' '深圳 5G' '移动'
+		 speed_test '60794' '广州 5G' '移动'
+		#***
+		 speed_test '30852' '昆山杜克大学'
+		 speed_test '69872' '西交利物浦大学'
 
 		end=$(date +%s)  
 		rm -rf speedtest*
@@ -249,17 +214,19 @@ runtest() {
 		echo "ID    测速服务器信息       上传/Mbps   下载/Mbps   延迟/ms"
 		start=$(date +%s) 
 
+		 speed_test '5396' '江苏苏州５Ｇ' '电信'
+		 speed_test '36663' '江苏镇江５Ｇ' '电信'
+		 speed_test '59386' '浙江５Ｇ' '电信'
+		 speed_test '59387' '浙江５Ｇ' '电信'
+		 #
 		 speed_test '3633' '上海' '电信'
-		 speed_test '24012' '内蒙古呼和浩特' '电信'
-		 speed_test '27377' '北京５Ｇ' '电信'
-		 speed_test '29026' '四川成都' '电信'
-		 speed_test '29071' '四川成都' '电信'
-		 speed_test '17145' '安徽合肥５Ｇ' '电信'
+
+
+
 		 speed_test '27594' '广东广州５Ｇ' '电信'
 		 speed_test '27810' '广西南宁' '电信'
 		 speed_test '27575' '新疆乌鲁木齐' '电信'
 		 speed_test '26352' '江苏南京５Ｇ' '电信'
-		 speed_test '5396' '江苏苏州５Ｇ' '电信'
 		 speed_test '5317' '江苏连云港５Ｇ' '电信'
 		 speed_test '7509' '浙江杭州' '电信'
 		 speed_test '23844' '湖北武汉' '电信'
@@ -288,14 +255,15 @@ runtest() {
 		echo "ID    测速服务器信息       上传/Mbps   下载/Mbps   延迟/ms"
 		start=$(date +%s) 
 
+		 speed_test '43752' '北京５Ｇ' '联通'
+		 speed_test '24447' '上海5G' '联通'
+		 #
 		 speed_test '21005' '上海' '联通'
-		 speed_test '24447' '上海５Ｇ' '联通'
 		 speed_test '5103' '云南昆明' '联通'
 		 speed_test '5145' '北京' '联通'
 		 speed_test '5505' '北京' '联通'
 		 speed_test '9484' '吉林长春' '联通'
 		 speed_test '2461' '四川成都' '联通'
-		 speed_test '27154' '天津５Ｇ' '联通'
 		 speed_test '5509' '宁夏银川' '联通'
 		 speed_test '5724' '安徽合肥' '联通'
 		 speed_test '5039' '山东济南' '联通'
@@ -332,54 +300,11 @@ runtest() {
 		echo "ID    测速服务器信息       上传/Mbps   下载/Mbps   延迟/ms"
 		start=$(date +%s) 
 
-		 speed_test '30154' '上海' '移动'
-		 speed_test '25637' '上海５Ｇ' '移动'
-		 speed_test '26728' '云南昆明' '移动'
-		 speed_test '27019' '内蒙古呼和浩特' '移动'
-		 speed_test '30232' '内蒙呼和浩特５Ｇ' '移动'
-		 speed_test '30293' '内蒙古通辽５Ｇ' '移动'
-		 speed_test '25858' '北京' '移动'
-		 speed_test '16375' '吉林长春' '移动'
-		 speed_test '24337' '四川成都' '移动'
-		 speed_test '17184' '天津５Ｇ' '移动'
-		 speed_test '26940' '宁夏银川' '移动'
-		 speed_test '31815' '宁夏银川' '移动'
-		 speed_test '26404' '安徽合肥５Ｇ' '移动'
-		 speed_test '27151' '山东临沂５Ｇ' '移动'
-		 speed_test '25881' '山东济南５Ｇ' '移动'
-		 speed_test '27100' '山东青岛５Ｇ' '移动'
-		 speed_test '26501' '山西太原５Ｇ' '移动'
-		 speed_test '31520' '广东中山' '移动'
-		 speed_test '6611' '广东广州' '移动'
-		 speed_test '4515' '广东深圳' '移动'
-		 speed_test '15863' '广西南宁' '移动'
-		 speed_test '16858' '新疆乌鲁木齐' '移动'
-		 speed_test '26938' '新疆乌鲁木齐５Ｇ' '移动'
-		 speed_test '17227' '新疆和田' '移动'
-		 speed_test '17245' '新疆喀什' '移动'
-		 speed_test '17222' '新疆阿勒泰' '移动'
-		 speed_test '27249' '江苏南京５Ｇ' '移动'
-		 speed_test '21845' '江苏常州５Ｇ' '移动'
-		 speed_test '26850' '江苏无锡５Ｇ' '移动'
-		 speed_test '17320' '江苏镇江５Ｇ' '移动'
-		 speed_test '25883' '江西南昌５Ｇ' '移动'
-		 speed_test '17223' '河北石家庄' '移动'
-		 speed_test '26331' '河南郑州５Ｇ' '移动'
-		 speed_test '6715' '浙江宁波５Ｇ' '移动'
-		 speed_test '4647' '浙江杭州' '移动'
-		 speed_test '16503' '海南海口' '移动'
-		 speed_test '28491' '湖南长沙５Ｇ' '移动'
-		 speed_test '16145' '甘肃兰州' '移动'
-		 speed_test '16171' '福建福州' '移动'
-		 speed_test '18444' '西藏拉萨' '移动'
-		 speed_test '16398' '贵州贵阳' '移动'
-		 speed_test '25728' '辽宁大连' '移动'
-		 speed_test '16167' '辽宁沈阳' '移动'
-		 speed_test '17584' '重庆' '移动'
-		 speed_test '26380' '陕西西安' '移动'
-		 speed_test '29105' '陕西西安５Ｇ' '移动'
-		 speed_test '29083' '青海西宁５Ｇ' '移动'
-		 speed_test '26656' '黑龙江哈尔滨' '移动'
+		 speed_test '4575' '四川成都' '移动'
+		 speed_test '16204' '江苏苏州' '移动'
+		 speed_test '54312' '杭州 5G' '移动'
+		 speed_test '60584' '深圳 5G' '移动'
+		 speed_test '60794' '广州 5G' '移动'
 
 		end=$(date +%s)  
 		rm -rf speedtest*
